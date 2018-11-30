e845beb66c1f83a22d51b29c06bbf6cbc9f399c7
Altaf ur Rehman <altafcs@gmail.com>
2018-11-30 12:26:29 +0500

    Missing Contact POJO

---
+++ android/src/main/java/com/joshuapinter/ContactAccount.java
@@ -0,0 +1,40 @@
+package com.joshuapinter;
+
+public class ContactAccount {
+    String identifier;
+    String title;
+    String type;
+    String login;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
